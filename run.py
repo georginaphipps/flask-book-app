a847e9edc90d7992668b2cdd2faba89654f47e1e
georgina phipps <gphipps31@gmail.com>
2024-06-13 18:37:49 +0000

    connect sheet data via google api

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
 import os
+import gspread
+from google.oauth2.service_account import Credentials
 import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('betsys_sales')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
